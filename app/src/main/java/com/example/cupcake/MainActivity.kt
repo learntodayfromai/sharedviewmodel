ad1d40cc798f163e5333289ee16c143f4ace3a3e
Learn Today <learntodayfromai@gmail.com>
2023-05-15 12:41:51 -0700

    added navigation up button

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -17,6 +17,8 @@ package com.example.cupcake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -25,13 +27,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
  * Activity for cupcake order flow.
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main){
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHost.findNavController()
+        navController = navHost.findNavController()
 
         setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp() : Boolean{
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
